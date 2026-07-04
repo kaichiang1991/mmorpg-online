767eb9ebc07801f99c953a054046f8430ea16e74
kai <addie60111@gmail.com>
2026-07-05 01:13:53 +0800

    test(game): validate Attack properties in unit test

--- apps/server/src/modules/game/domain/attack.spec.ts
+++ apps/server/src/modules/game/domain/attack.spec.ts
@@ -2,8 +2,10 @@ import { Attack } from './attack';
 
 const makeAttack = () => new Attack(10);
 describe('Attack', () => {
-  it('create with base damaRge', () => {
+  it('create with base damage', () => {
     const attack = makeAttack();
     expect(attack.baseDamage).toBe(10);
+    expect(attack.multiplier).toBe(1);
+    expect(attack.type).toBe('physical');
   });
 });
